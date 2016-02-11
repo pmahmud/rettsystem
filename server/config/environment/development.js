2a885db3ec143a471333de9b6dce01b2a0ecb846
Pallab Mahmud <pallmahmud@gmail.com>
2016-02-10 23:58:37 -0800

    first commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/rettsystem-dev'
+  },
+
+  // Seed database on startup
+  seedDB: true
+
+};
